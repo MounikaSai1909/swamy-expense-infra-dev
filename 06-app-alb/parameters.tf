78eb94f58b90329d876c11add715c90efe3c71c9
Putti Swamy <puttiswamy123@gmail.com>
2024-12-29 15:40:13 +0530

    files

---
+++ 06-app-alb/parameters.tf
@@ -0,0 +1,6 @@
+resource "aws_ssm_parameter" "app-alb_listener_arn" {
+  name  = "/${var.project_name}/${var.environment}/app-alb_listener_arn"
+  type  = "String"
+  value = aws_lb_listener.http.arn
+
+}
